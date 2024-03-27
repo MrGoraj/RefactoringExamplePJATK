f10a712bd189116fcac054d7398bb8143119b5a5
MrGoraj <32809530+MrGoraj@users.noreply.github.com>
2024-03-27 17:41:40 +0100

    Code refactor

--- LegacyApp/ClientRepository.cs
+++ LegacyApp/ClientRepository.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
+﻿using System.Collections.Generic;
 
 namespace LegacyApp
 {
@@ -9,7 +7,7 @@ namespace LegacyApp
         /// <summary>
         /// This collection is used to simulate remote database
         /// </summary>
-        public static readonly Dictionary<int, Client> Database = new Dictionary<int, Client>()
+        private static readonly Dictionary<int, Client> Database = new Dictionary<int, Client>()
         {
             {1, new Client{ClientId = 1, Name = "Kowalski", Address = "Warszawa, Złota 12", Email = "kowalski@wp.pl", Type = "NormalClient"}},
             {2, new Client{ClientId = 2, Name = "Malewski", Address = "Warszawa, Koszykowa 86", Email = "malewski@gmail.pl", Type = "VeryImportantClient"}},
@@ -18,24 +16,15 @@ namespace LegacyApp
             {5, new Client{ClientId = 5, Name = "Kwiatkowski", Address = "Warszawa, Złota 52", Email = "kwiatkowski@wp.pl", Type = "NormalClient"}},
             {6, new Client{ClientId = 6, Name = "Andrzejewicz", Address = "Warszawa, Koszykowa 52", Email = "andrzejewicz@wp.pl", Type = "NormalClient"}}
         };
-        
-        public ClientRepository()
-        {
-        }
 
         /// <summary>
-        /// Simulating fetching a client from remote database
+        /// Fetching a client from remote database
         /// </summary>
-        /// <returns>Returning client object</returns>
+        /// <returns>Returning client object or null if not found</returns>
         internal Client GetById(int clientId)
         {
-            int randomWaitTime = new Random().Next(2000);
-            Thread.Sleep(randomWaitTime);
-
-            if (Database.ContainsKey(clientId))
-                return Database[clientId];
-
-            throw new ArgumentException($"User with id {clientId} does not exist in database");
+            Database.TryGetValue(clientId, out var client);
+            return client;
         }
     }
 }
--- LegacyApp/UserCreditService.cs
+++ LegacyApp/UserCreditService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 
 namespace LegacyApp
 {
@@ -18,7 +17,7 @@ namespace LegacyApp
                 {"Doe", 3000},
                 {"Kwiatkowski", 1000}
             };
-        
+
         public void Dispose()
         {
             //Simulating disposing of resources
@@ -27,16 +26,11 @@ namespace LegacyApp
         /// <summary>
         /// This method is simulating contact with remote service which is used to get info about someone's credit limit
         /// </summary>
-        /// <returns>Client's credit limit</returns>
+        /// <returns>Client's credit limit or 0 if not found</returns>
         internal int GetCreditLimit(string lastName, DateTime dateOfBirth)
         {
-            int randomWaitingTime = new Random().Next(3000);
-            Thread.Sleep(randomWaitingTime);
-
-            if (_database.ContainsKey(lastName))
-                return _database[lastName];
-
-            throw new ArgumentException($"Client {lastName} does not exist");
+            _database.TryGetValue(lastName, out var creditLimit);
+            return creditLimit;
         }
     }
 }

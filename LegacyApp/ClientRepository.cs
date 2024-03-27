12747c32fdedfa7b29368af2af2780d746af2a5b
MrGoraj <32809530+MrGoraj@users.noreply.github.com>
2024-03-27 17:41:40 +0100

    Code refactor

--- LegacyApp/ClientRepository.cs
+++ LegacyApp/ClientRepository.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
+﻿using System.Collections.Generic;
 
 namespace LegacyApp
 {
@@ -9,7 +7,7 @@ namespace LegacyApp
         /// <summary>
         /// This collection is used to simulate remote database
         /// </summary>
-        public static readonly Dictionary<int, Client> Database = new Dictionary<int, Client>()
+        private static readonly Dictionary<int, Client> Database = new Dictionary<int, Client>()
         {
             {1, new Client{ClientId = 1, Name = "Kowalski", Address = "Warszawa, Złota 12", Email = "kowalski@wp.pl", Type = "NormalClient"}},
             {2, new Client{ClientId = 2, Name = "Malewski", Address = "Warszawa, Koszykowa 86", Email = "malewski@gmail.pl", Type = "VeryImportantClient"}},
@@ -24,18 +22,13 @@ namespace LegacyApp
         }
 
         /// <summary>
-        /// Simulating fetching a client from remote database
+        /// Fetching a client from remote database
         /// </summary>
-        /// <returns>Returning client object</returns>
+        /// <returns>Returning client object or null if not found</returns>
         internal Client GetById(int clientId)
         {
-            int randomWaitTime = new Random().Next(2000);
-            Thread.Sleep(randomWaitTime);
-
-            if (Database.ContainsKey(clientId))
-                return Database[clientId];
-
-            throw new ArgumentException($"User with id {clientId} does not exist in database");
+            Database.TryGetValue(clientId, out var client);
+            return client;
         }
     }
 }
